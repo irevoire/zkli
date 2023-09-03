604db23147f273430a32793c318716169e9a48ed
Tamo <tamo@meilisearch.com>
2023-09-03 21:50:20 +0200

    improve the error message

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@ use std::{
 };
 
 use clap::Parser;
+use miette::{IntoDiagnostic, Result};
 use zookeeper::ZooKeeper;
 
 #[derive(Debug, Parser)]
 #[clap(about = "Cli around zookeeper")]
 struct Options {
-    #[clap(default_value_t = String::from("localhost:2181/"))]
-    pub url: String,
+    #[clap(long, short, default_value_t = String::from("localhost:2181/"))]
+    pub addr: String,
 
     #[clap(subcommand)]
     pub command: Command,
@@ -24,22 +25,22 @@ enum Command {
     Rm { file: String },
 }
 
-fn main() {
+fn main() -> Result<()> {
     let opt = Options::parse();
-    // env_logger::init();
     let mut log_builder = env_logger::Builder::new();
     log_builder.parse_filters("info");
 
     log_builder.init();
 
-    let zk = ZooKeeper::connect(&opt.url, Duration::from_secs(1), |_| ()).unwrap();
+    log::info!("Connecting to {}", opt.addr);
+    let zk = ZooKeeper::connect(&opt.addr, Duration::from_secs(1), |_| ()).into_diagnostic()?;
     log::info!("Connected");
 
     match opt.command {
         Command::Ls { path } => {
             let mut path = path.unwrap_or(String::from("/"));
             sanitize_path(&mut path);
-            let mut children = zk.get_children(&path, false).unwrap();
+            let mut children = zk.get_children(&path, false).into_diagnostic()?;
             children.sort();
             for child in children {
                 if path == "/" {
@@ -47,7 +48,7 @@ fn main() {
                 }
                 let stat = zk
                     .exists(&format!("{}/{}", path, child), false)
-                    .unwrap()
+                    .into_diagnostic()?
                     .unwrap();
                 if stat.num_children == 0 {
                     print!("{child} ");
@@ -60,24 +61,25 @@ fn main() {
         Command::Tree { path } => {
             let mut path = path.unwrap_or(String::from("/"));
             sanitize_path(&mut path);
-            tree(&zk, &path, 0);
+            tree(&zk, &path, 0)?;
         }
         Command::Cat { mut file } => {
             sanitize_path(&mut file);
-            let (data, _) = zk.get_data(&file, false).unwrap();
-            stdout().write_all(&data).unwrap();
+            let (data, _) = zk.get_data(&file, false).into_diagnostic()?;
+            stdout().write_all(&data).into_diagnostic()?;
         }
         Command::Rm { mut file } => {
             sanitize_path(&mut file);
-            zk.delete(&file, None).unwrap();
+            zk.delete(&file, None).into_diagnostic()?;
             log::info!("Successfully deleted");
         }
     }
+    Ok(())
 }
 
-fn tree(zk: &ZooKeeper, mut path: &str, depth: usize) {
+fn tree(zk: &ZooKeeper, mut path: &str, depth: usize) -> Result<()> {
     println!("{}{}", "  ".repeat(depth), path);
-    let mut children = zk.get_children(&path, false).unwrap();
+    let mut children = zk.get_children(&path, false).into_diagnostic()?;
     if path == "/" {
         path = ""
     }
@@ -85,14 +87,15 @@ fn tree(zk: &ZooKeeper, mut path: &str, depth: usize) {
     for child in children {
         let stat = zk
             .exists(&format!("{}/{}", path, child), false)
-            .unwrap()
+            .into_diagnostic()?
             .unwrap();
         if stat.num_children == 0 {
             println!("{}{child}", "  ".repeat(depth + 1));
         } else {
-            tree(zk, &format!("{path}/{child}"), depth + 1);
+            tree(zk, &format!("{path}/{child}"), depth + 1)?;
         }
     }
+    Ok(())
 }
 
 fn sanitize_path(path: &mut String) {
